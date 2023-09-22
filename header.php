936f8b1e4ace6bf68a40de4233fa33643ff6f626
Taylor Jadin <jadin.taylor@gmail.com>
2023-09-22 12:39:32 -0500

    header

---
+++ header.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WordPress
+ * @subpackage Twenty_Nineteen
+ * @since Twenty Nineteen 1.0
+ */
+?><!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<link rel="profile" href="https://gmpg.org/xfn/11" />
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
+<div id="page" class="site">
+	<a class="skip-link screen-reader-text" href="#content">
+		<?php
+		/* translators: Hidden accessibility text. */
+		_e( 'Skip to content', 'twentynineteen' );
+		?>
+	</a>
+
+    <header class="navbar">
+    		<nav class="container-fluid bd-gutter flex-wrap flex-lg-nowrap" aria-label="Main navigation">
+    			<a href="https://events.reclaimhosting.com/"><img width="405" height="70" src="https://community.reclaimhosting.com/imgs/reclaim-logo.png" alt="Relcaim Hosting logo." class="header-logo img-fluid"></a>
+            <ul id="nav-menu">
+              <li>
+                <a href="https://events.reclaimhosting.com/events" class="">
+                  Event Calendar
+                </a>
+              </li>
+              <li>
+                <a href="https://community.reclaimhosting.com" class="">
+                  Reclaim Community
+                </a>
+              </li>
+              <li>
+                <a href="https://reclaimhosting.com/about" class="">
+                  About Reclaim Hosting
+                </a>
+              </li>
+              <li>
+                <a href="https://reclaimed.tech/discord" class="">
+                  Say Hello on Discord
+                </a>
+              </li>
+              <li>
+                <a href="https://support.reclaimhosting.com/hc/en-us/requests/new" class="">
+                  Get in Touch
+                </a>
+              </li>
+            </ul>
+  			</nav>
+    	</header>
+<div class="divider"></div>
+
+<style>
+.wp-site-blocks {
+  padding-top: 0 !important;
+}
+</style>
+
+	<div id="content" class="site-content">
